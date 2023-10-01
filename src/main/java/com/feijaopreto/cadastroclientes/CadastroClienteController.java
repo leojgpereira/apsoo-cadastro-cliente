253a5670f6a6fb6b3173d5158e370f8c7d00c954
Leandro Pereira <leojgpereira@gmail.com>
2023-10-01 13:44:06 -0400

    Renomeia nomes dos arquivos

--- src/main/java/com/feijaopreto/cadastroclientes/CadastroClienteController.java
+++ src/main/java/com/feijaopreto/cadastroclientes/CadastroClienteController.java
@@ -3,7 +3,7 @@ package com.feijaopreto.cadastroclientes;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CadastroClienteController {
     @FXML
     private Label welcomeText;
 
